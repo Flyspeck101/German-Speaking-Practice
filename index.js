af9392d2ad77600e254715b9a85a101ff18474a3
Flyspeck101 <lawrence.chen8@education.nsw.gov.au>
2024-05-22 11:27:35 +1000

    Updated for 2024

--- index.js
+++ index.js
@@ -1,91 +1,208 @@
 
-var questionsText = (`Wo wohnst du?
-Was gibt es alles in deinem Vorort?
-Was kann man in SUBURB machen?
-Kann man in deinem Wohnort ACTIVITY?
-Wo ist der/die/das .............. in deinem Wohnort?
-Gibt es eine/einen ............ in deinem Wohnort?
-In welcher Straße wohnst du?
-Wohnst du in der Nähe von einem/einer PLACE?
-Wie kommst du zur/zum ............................?
-Wie lange dauert das? Wie viele Kilometer sind das?
-Wie lange brauchst du von ............. bis ..............?
-Wie weit ist es von ............. bis ..............?
-Wohnst du gern in Sydney? Warum?
-Wohnst du gern in deinem Wohnort? Warum?
-Wo wohnt dein bester Freund?
-Was machst du gern im ..................(seasons)?
-Was machst du gern am ................... (Wochenende, Morgen, Nachmittag, Abend, …..tag)?
-Wohin gehst du am ...................... (Wochenende, Morgen, Nachmittag, Abend, …..tag)?\
-Wohin gehst du am ................... (Wochenende, Morgen, Nachmittag, Abend, …..tag)?
-Kannst/Darfst/Willst/Möchtest ACTIVITY?
-Was isst und trinkst du gern?
-Was isst du zum Frühstück?
-Was isst du zum Mittagessen? 
-Nimmst du es von ZuHause mit oder kaufst du es in der Schule?
-Wieviel kostet in deiner Schule DRINK/FOOD?
-Findest du das teuer oder billig?
-Was trägst du gern?
-Sind Kleider wichtig für dich?
-Welche Größe hast du?
-Wo kaufst du deine Kleider?
-Wo kaufst du am liebsten ein?
-Wer kauft deine Kleider?
-Kaufst du gern Kleider im Sonderangebot?
-Was trägst du heute?
-Was trägst du gern am Wochenende?
-Was trägst du im Sommer/ Winter?
-Was tragen junge Leute zu Partys?
+var questionsText = (`Was für eine Schule besuchst du? 
+Bitte beschreibe deine Schule. Wie sieht sie aus?
+Ist deine Schule eine gute Schule? Warum/nicht?
+Gehst du gern zur Schule? 
+Was gefällt dir an deiner Schule? 
+Was gefällt dir nicht an deiner Schule? 
+Wie könnte deine Schule besser sein? Was würdest du an deiner Schule ändern, wenn du könntest?
+Wie kommst du zur Schule? Wie lange dauert das?
+Was machst du auf dem Weg zur Schule?
+Wie sind deine Schulkameraden? 
+Hast du einen guten Freundeskreis?
+Wie sind die Lehrer deiner Schule? 
+Welche Fächer lernst du in der Oberstufe und warum (you must be able to explain why you do each subject).
+Wurdest du gezwungen, diese Fächer zu machen?
+Konntest du selbst über deine Fächer entscheiden?
+Was ist dein Lieblingsfach? Warum?
+Warum lernst du Deutsch? 
+Ist es deiner Meinung nach wichtig, eine Fremdsprache zu lernen? 
+Wäre es besser, eine asiatische Sprache zu lernen?
+Warum ist das Erlernen einer Fremdsprache in Australien nicht so populär wie in Europa?
+In welchem Fach bekommst du die besten Noten? 
+In welchem Fach bekommst du die schlechtesten Noten?
+Wie war dein letztes Zeugnis? 
+Wie reagieren deine Eltern, wenn du ein schlechtes Zeugnis mit nach Hause bringst?
+Helfen dir deine Eltern mit der Schularbeit? Wie?
+Es wird oft behauptet, dass Mädchen besser lernen und erfolgreicher sind. Stimmt das?
+Es wird oft behauptet, dass Mädchen im HSC einen Vorteil haben. Stimmst du zu?
+Was hälst du davon, dass die Schüler in Deutschland keine Uniform tragen?
+Würdest du auch gern keine Uniform tragen?
 Beschreib deine Schuluniform.
-Wie findest du Jeansjacken?/ Flip Flops?/ karierte Hemden? Etc.
-Was findest du besser: ______ oder ________? Warum?
-Wie findest du die Mode im Moment?
-Ist Mode wichtig fuer dich? Warum?
-Was trägt dein Vater/ deine Mutter gern?
-Was tragen deine Lehrer?
-Was tragen deine Freunde?
-Kannst du dich beschreiben?
-Wie siehst du aus?
-Kannst du deine/deinen ………………...beschreiben ?
-Wie sieht dein/deine ………..... aus?
-Wie gross bist du?
-Wie gross ist dein/deine …….? 
-Wie ist das Wetter heute?
-Wie ist die Temperatur heute in Sydney?
-Was ist dein Traum? (Was willst du in der Zukunft machen?)
-Wohnst du in einer Wohnung oder einem Haus?
-Kannst du dein Haus /deine Wohnung beschreiben?
-Wo ist dein Zimmer? 
-Was ist neben, unter, hinter … deinem Zimmer? 
-Ist dein Zimmer im ersten/zweiten… Stock?
-Was gibt es alles in deinem Zimmer? 
-Was machst du normalerweise in deinem Zimmer?
-Was kann man aus deinem Fenster sehen?
-Hast du ein Lieblingszimmer?
-Welche Zimmer sind im ersten/zweiten Stock?
-Hast du einen Keller/Dachboden? Was ist im Keller/Dachboden?
-Welches Zimmer ist unter/neben/vor/hinter dem/der ...................... (room in your house)?
-Musst du im Haushalt helfen? Was musst du machen?
-Wie oft musst du ............... (a chore)?
-Was ist leichter, …..oder ………?
-Was ist besser, …..oder ………? 
-Was macht dein Vater/ deine Schwester?/ dein Bruder im Haushalt?
-Welche Geschäfte gibt es in deinem Wohnort?
-Gibt es in deinem Wohnort ein/eine ..........................?
-Wo ist der/die/das .......................in deinem Wohnort?
-Welchen TZJ würdest du gerne haben?
-Erzähl mir von deinem Teilzeitjob! Wie verdienst du Geld?
-Wieviel verdienst du pro Stunde/Woche/Monat?
-Was machst du mit deinem Geld/Taschengeld? Wofür gibst du dein Geld aus?
-Sparst du auch? 
-Wofür sparst du?
-Wofür gibst du am meisten aus? 
-Hat dein Freund einen TZJ? Was machen er?
-Was darf man in Australien/Deutschland machen / nicht machen?
-Was darfst du zu Hause machen / nicht machen?
-Was darfst du in deiner Schule machen / nicht machen?
-Wie findest du den/die/das ………………….?
-`);
+Gingst du lieber auf ein Internat? Warum/nicht?
+Was war bisher deine beste Erfahrung in der Schule? Erzähl ein wenig.
+Was war bisher deine schlechteste Erfahrung in der Schule? Erzähl ein wenig.
+Hast du ein Vorbild an der Schule?
+Wie muss ein guter Lehrer für dich sein?
+Wie ist ein schlechter Lehrer?
+Eine Schule steht und fällt , d.h. ist so gut wie der Schulleiter. Würdest du zustimmen?
+Habt ihr viele AGs/Vereine an der Schule? Bist du Teil einer AG/eines Vereins?
+Findest du es wichtig, dass eine Schule viele außerschulische  Aktivitäten anbietet?
+Interessierst du dich für was deine die Schule außerhalb des Unterrichts anbietet?
+Was muss man deiner Meinung nach machen, um ein gutes HSC zu bekommen?
+Wie wichtig ist es für dich eine gute Note im HSC zu bekommen?
+Kannst du das das deutsche Schulsystem beschreiben?
+Wie findest du das australische Schulsystem? 
+Findest du das australische oder deutsche Schulsystem besser? Warum?
+Warst du schon mal auf einer deutschen Schule? 
+Würdest du gern eine deutsche Schule besuchen?
+Welche Schüler besuchen in Deutschland ein Gymnasium?
+Denkst du, dass es gut ist, dass sehr intelligente Schüler getrennt lernen können?
+Was können Schüler machen, die nicht gut genug für ein Gymnasium sind?
+Die meisten deutschen Schulen sind gemischt. Wie findest du das? 
+Was sind die Vor/ Nachteile einer reinen Jungenschule / Mädchenschule?
+Wärst du lieber an einer gemischten Schule? 
+Gibt es in deiner Schule Schüler mit Behinderungen? Lernt ihr zusammen oder getrennt?
+Welche Zukunftspläne hast du? Was willst du nach der Schule machen? 
+Warum machen manche Leute ein Zwischenjahr nach dem HSC?  
+Glaubst du es ist eine gute Idee ein Zwischenjahr zu machen?
+Wirst du ein Zwischenjahr machen oder gehst du direkt auf die Uni?
+Wie wirst du deinen Schulabschluss feiern?
+Wohin willst du am liebsten reisen? 
+Willst du ins Ausland reisen? 
+Willst du um Australien reisen?
+Möchtest du im Ausland arbeiten? 
+Möchtest du im Ausland leben? 
+Wo würdest du später am liebsten wohnen? 
+Was möchtest du später beruflich machen? Welche Berufspläne hast du? Was willst du einmal werden?
+Hast du diesen Beruf gewählt oder haben deine Eltern dich beeinflusst?
+Haben Eltern deine Eltern einen Einfluss auf deine Berufswahl?
+Welche Berufe haben deine Eltern/ Großeltern? Hat dich das beeinflusst?
+Was ist für dich das Wichtigste an einem Beruf?
+Spielt Geld eine wichtige Rolle für dich in der Berufswahl?
+Hast du in der 10. Klasse ein Berufspraktikum gemacht? Wie war es? 
+Würdest du das Berufspraktikum anderen Schülern empfehlen?
+Was willst du direkt nach dem HSC machen?
+Willst du an der Uni studieren? 
+Wie lange wird dein Studium dauern? 
+Wenn du keinen Platz an der Uni bekommen würdest, was würdest du machen?
+Wären deine Eltern sehr enttäuscht, wenn du nicht studieren würdest? Warum/nicht?
+Könntest du dir vorstellen, im Ausland zu studieren?
+Könntest du dir vorstellen, nach dem HSC eine Lehre zu machen?
+Wirst du nach dem HSC weiterhin Deutsch lernen?
+Wie waere fuer dich ein Studium in Deutschland?
+Wann hast/bekommst/kriegst du Ferien? Wie lange dauern diese Ferien?
+Wie findest du Ferien? 
+Was machst du normalerweise in den Ferien/im Urlaub?
+Wohin reist du normalerweise im Sommer / im Winter / im Herbst / im Frühling?
+Reist du normalerweise ins Ausland oder bleibst du in Australien? Warum?
+Warst du schon mal im Ausland? Ja - wo? Nein - Wohin würdest du gerne reisen?
+Bist du schon einmal in Australien gereist? Ja - wohin? Nein - Wohin würdest du gerne reisen?
+Machst du lieber in der Stadt oder auf dem Land Urlaub? Warum?
+Reist du lieber in die Berge oder ans Meer? Warum?
+Würdest du deine Ferien  lieber in einem Zelt oder in einem Hotel verbringen? Warum?
+Gehst du lieber mit oder ohne deine Familie in Urlaub?
+Wie oft fährt deine Familie in den Urlaub? Fährst du immer mit?
+Fährst du manchmal am Wochenende mit deinen Freunden weg? 
+Ja - Wohin fahrt ihr? Was macht ihr zusammen in den Ferien? Nein - Warum nicht?
+Beschreibe einen schönen Urlaub.Hast du eine schöne Urlaubserinnerung? 
+Was hast du in den letzten Ferien / im letzten Urlaub gemacht? 
+Mit wem warst du im Urlaub? Warst du alleine? Bist du mit deinen Eltern / deiner Familie gefahren? 
+Wohin bist du letzten Sommer / Winter / Herbst /  Frühling gereist /gefahren?
+Was hast du dort gemacht?
+Was hast du gesehen?
+Was hat dir am besten gefallen? Was hat keinen Spass gemacht?
+Wie war das Essen / das Wetter? 
+Hat dir der Urlaub gefallen?
+Bist du schon einmal Ski gefahren? Wie hast du die Kälte gefunden?
+Hast du deinen Urlaub schon einmal an einem Strand verbracht? Wie findest du die Sonne und die Hitze?
+Bist du schon einmal Zelten gegangen? Wie war es?
+Warst du schon einmal in Deutschland / in der Schweiz / in Österreich?
+Möchtest du Deutschland / die Schweiz / Österreich  besuchen? Warum?
+Wie lange würdest du in bleiben? 
+Was möchtest du dort  machen?
+Kannst du dir vorstellen, eines Tages dort zu wohnen?
+Möchtest du einmal bei einem Austausch mitmachen? Warum? Warum nicht?
+Wo würdest du am liebsten Urlaub machen? Was ist dein Traumreiseziel?
+Was möchtest du am Ende des Jahres machen?  Hast du Pläne nach den Examen / dem HSC / dem Abitur? 
+Was ist der schönste Urlaub, den du gemacht hast?
+Was ist der schlimmste Urlaub, den du gehabt hast?
+Was machst du in den Ferien, wenn du nicht viel Geld hast?
+Wenn du ganz viel Geld hättest, was würdest du im Ausland unternehmen?
+Wohin bist du als Kind gern in den Urlaub gefahren?
+Wie wichtig ist es, die Sprache des Reiselandes zu sprechen?
+Warst du selbst schon einmal ein Austauschschüler? Hast du schon einmal einen Austausch gemacht?
+Was machst du gern in deiner Freizeit / am Wochenende / nach der Schule/  wenn du Zeit hast?
+Was machst du am liebsten, wenn du Zeit hast?
+Hast du Hobbies? 
+Hast du Zeit für Hobbys? Hast du (viel/genug) Freizeit?
+Welche Hobbys hattest du früher? Welche Hobbys hattest du, als du jünger warst?
+Liest du gern? 
+Was liest du gern? 
+Hast du ein Lieblingsbuch? 
+Kannst du mir ein bisschen über dieses Buch erzählen? 
+Gehst du ins Kino? 
+Wie oft gehst du ins Kino? 
+Welche Filme siehst du gern? 
+Hast du einen Lieblingsfilm? 
+Hast du einen Lieblingsschauspieler? 
+Hörst du gern Musik? 
+Was für Musik hörst du am liebsten? 
+Bist du musikalisch? 
+Spielst du ein Instrument? 
+Musst du üben? 
+Wie oft musst du üben? 
+Spielst du in einer Band oder in einem Orchester? 
+Spielst du Computerspiele? Magst du zocken?
+Wie viele Stunden spielst du pro Tag?
+Kosten deine Computerspiele Geld?
+Gehst du gern auf Partys?
+Kannst du die letzte Party beschreiben, auf die du gegangen bist?
+Warum gehen so viele Jugendliche gern auf Partys?
+Hast du je eine Party gegeben? Wie hast du die Party organisiert? Was ist passiert?
+Wann machen Partys gar keinen Spaß?
+Gehst du gern ins Theater?
+Hast du neulich ein Theaterstück gesehen?
+Hat dir das Theaterstück gut gefallen?
+Gehst du gern zu Ausstellungen?
+Hast du neulich eine gute Ausstellung gesehen?
+Wie hat dir die Ausstellungen gefallen?
+Gehst du gerne zum Strand?
+Wie kann man sich am Strand amüsieren?
+Wohnst du weit vom Strand entfernt? Wie kommst du dorthin?
+Gefallen dir Wassersportarten?
+Sammelst du etwas? 
+Wo findest du es? 
+Ist es teuer oder billig?
+Siehst du gern fern?
+Wann siehst du fern?
+Welche Sendungen siehst du am liebsten?
+Welche Sendungen findest du uninteressant?
+Meinst du, dass das Fernsehen einen schlechten Einfluss haben kann?
+Siehst du gern E-Sport?
+Warum interessiert du dich dafür?
+Spielst du Schach? Wann? Wo? Mit wem?
+Spielst du Brettspiele? Welche? Mit wem? Wann?
+Beschreibe einen typischen Tag, an dem du Freizeit hast.
+Bist du sportlich? 
+Treibst du Sport? Interessierst du dich für Sport?
+Welchen Sport treibst du?
+Macht das Spaß?
+Wie findest du SPORT? Für welche Sportarten interessierst du dich? Interessierst du dich für SPORT?
+Bist du Mitglied einer Mannschaft? Bist du in einer Mannschaft? 
+Wann ist das Training?
+Wie oft trainierst du? 
+Wie lange dauert das Training?
+Wann finden deine Wettbewerbe statt? 
+Wann spielt ihr gewöhnlich? 
+Welchen Sport würdest du ausüben, wenn du mehr Zeit hättest?
+Machst du gern anstrengende Sportarten?
+Bist du selbst aktiv oder lieber Zuschauer?
+Hast du je wegen Sport einen Unfall gehabt? Was ist passiert?
+Kann man in der 12ten Klasse  fit und gesund sein?
+Isst du gesundes Essen? Wer kocht es?
+Isst du manchmal Fast Food? Warum?
+Leidest du unter Stress? Bist du gestresst? 
+Findest du dieses Jahr stressig?
+Was machst du, um dich zu entspannen?
+Was kann man machen, um fit und gesund zu bleiben?
+Was machst du, um fit und gesund zu bleiben? Wie hältst du dich fit und gesund?
+Ist es dir wichtig, fit zu sein?
+Warum ist es wichtig, fit zu bleiben?
+Gehst du joggen? 
+Gehst du ins Fitnessstudio? 
+Schwimmst du? 
+Meditierst du?`);
 
 
   const questions = questionsText.split("\n");
